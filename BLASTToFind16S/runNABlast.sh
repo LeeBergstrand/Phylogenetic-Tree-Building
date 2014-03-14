bc60f38183d5d9937c86d113bb0b5df826aae243
Lee Bergstrand <leemacboy@gmail.com>
2014-03-14 12:00:10 -0700

    Renamed Example BLAST DB. improved runBLAST.sh

--- BLASTToFind16S/runNABlast.sh
+++ BLASTToFind16S/runNABlast.sh
@@ -4,7 +4,7 @@
 for fasta;
 do
 	echo Running blast on $fasta
-	blastp -db $fasta -query SteriodQueryV3.faa -out "$fasta.csv" -evalue 1e-40 -num_threads 16 -outfmt "10 qseqid sseqid pident evalue qcovhsp score" 
+	blastn -db ./Example16DB/RDBActinoBacteria16S.fna -query $fasta -max_target_seqs 1 -outfmt "10 qseqid sseqid length qseq evalue bitscore"
 done
 echo All files BLASTed.
 exit 0
