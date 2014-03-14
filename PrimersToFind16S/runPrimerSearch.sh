d2af2605a22501627259446fd8385e3416196611
Lee Bergstrand <leemacboy@gmail.com>
2014-03-14 16:53:40 -0700

    Added info for problem genomes.

---
+++ PrimersToFind16S/runPrimerSearch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# A simple script for the batch creation of blast databases from a directory with fasta files.
+
+for fasta;
+do
+	echo "Searching for primer hits in $fasta"
+	echo "	Searching for the 27F Bacteria 16S rRNA Primer..."
+	grep "AGAGTTTGATC[AC]TGGCTCAG" $fasta
+	echo "	Searching for the 1492R Bacteria 16S rRNA Primer..."
+	grep "GGTTACCTTGTTACGACTT" $fasta
+	echo "	Searching for the 907R Bacteria 16S rRNA Primer..."
+	grep "CCGTCAATTC[AC]TTTGAGTTT" $fasta
+	echo "	Searching for the 63F Bacteria 16S rRNA Primer..."
+	grep "CAGGCCTAACACATGCAAGTC" $fasta
+	echo "	Searching for the 357F Bacteria 16S rRNA Primer..."
+	grep "CCTACGGGAGGCAGCAG" $fasta
+	echo "	Searching for the 518R Universal 16S rRNA Primer..."
+	grep "ATTACCGCGGCTGCTGG" $fasta
+	done
+echo All files searched.
+exit 0
+
